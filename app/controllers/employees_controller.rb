3bface424fe387424aa5e613b41028d80638615f
Vishhh <vishwasn.pis@gmail.com>
2015-11-17 14:56:46 +0530

    added employee action

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,23 @@
 class EmployeesController < ApplicationController
+	def index
+	@employees= Employee.all
+	end
+	def new
+		@employee= Employee.new
+	end
+	def show
+		@employee = Employee.find(params[:id])
+	end
+	def edit
+		@employee = Employee.find(params[:id])
+	end
+  def update
+	  @employee = Employee.find(params[:id])
+		  if @employee.update
+		    redirect_to @employee
+		  else
+		    render 'edit'
+		  end 
+	  end
+
 end
