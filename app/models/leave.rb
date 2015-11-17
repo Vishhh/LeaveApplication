27b5bc378978ede249c00ecddb939a9d0558b883
Vishhh <vishwasn.pis@gmail.com>
2015-11-17 18:20:36 +0530

    adding Validations to Leave

--- app/models/leave.rb
+++ app/models/leave.rb
@@ -1,7 +1,7 @@
 class Leave < ActiveRecord::Base
 	belongs_to :employee, foreign_key: "employee_id"
   belongs_to :user
-
+	validates :leave_from, :leave_to, presence: true
   def generate_leave_number(user)
     exiting_leave_count = Leave.count
     self.leave_number = ("%03d"%(exiting_leave_count+1))
