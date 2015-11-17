761f1330bf7a5c96fd5fecf3c198093a0394e7b5
Vishhh <vishwasn.pis@gmail.com>
2015-11-17 14:59:06 +0530

    conditions for user and employee

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+   def after_sign_in_path_for(resource)
+		if (resource == current_user)
+		  users_path
+	  elsif (resource == current_employee)
+	    employees_path
+		end
+	end
 end
