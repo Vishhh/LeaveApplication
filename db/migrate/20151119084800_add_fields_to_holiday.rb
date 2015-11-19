35611c7b5aed631e3f59017641298f8cd8f01f2f
VishwasN <vishwasn.pis@gmail.com>
2015-11-19 16:01:41 +0530

    template changes

--- db/migrate/20151119084800_add_fields_to_holiday.rb
+++ db/migrate/20151119084800_add_fields_to_holiday.rb
@@ -1,6 +1,6 @@
 class AddFieldsToHoliday < ActiveRecord::Migration
   def change
-    add_column :holidays, :desctiption, :string
+    add_column :holidays, :description, :string
     add_column :holidays, :leave_from, :date
     add_column :holidays, :leave_to, :date
     add_column :holidays, :user_id, :integer
