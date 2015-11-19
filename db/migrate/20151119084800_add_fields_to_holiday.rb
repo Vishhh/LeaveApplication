0e6c70238be02bdf502b60d8921fad86aeb4c70d
VishwasN <vishwasn.pis@gmail.com>
2015-11-19 14:31:19 +0530

    fields in holidays

--- db/migrate/20151119084800_add_fields_to_holiday.rb
+++ db/migrate/20151119084800_add_fields_to_holiday.rb
@@ -1,7 +1,9 @@
 class AddFieldsToHoliday < ActiveRecord::Migration
   def change
     add_column :holidays, :desctiption, :string
-    add_column :holidays, :leave_from, :string
-    add_column :holidays, :leave_to, :string
+    add_column :holidays, :leave_from, :date
+    add_column :holidays, :leave_to, :date
+    add_column :holidays, :user_id, :integer
+    add_column :holidays, :employee_id, :integer
   end
 end
