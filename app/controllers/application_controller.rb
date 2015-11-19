17426109e61622ad76df3ccbcee65a744471b3d2
VishwasN <vishwasn.pis@gmail.com>
2015-11-19 16:16:37 +0530

    added name field in employee + permit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
+	end
+
  include CanCan::ControllerAdditions
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
