ce8b57f323b4cf69e2c770bf9c454e700aee4e2e
VishwasN <vishwasn.pis@gmail.com>
2015-11-24 17:04:05 +0530

    removing extra routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
   resources :users
   resources :holidays
   resources :hardwares
-  resources :salaries
   resources :leaves do
     member do
       patch 'approve'
