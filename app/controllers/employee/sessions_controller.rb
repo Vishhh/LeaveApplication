f8b1f647bfb5cabd4813fb83aa003f37d108a471
VishwasN <vishwasn.pis@gmail.com>
2015-11-28 12:09:40 +0530

    overriding the controller

--- app/controllers/employee/sessions_controller.rb
+++ app/controllers/employee/sessions_controller.rb
@@ -7,9 +7,13 @@ class Employee::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+   def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message(:notice, :signed_in) if is_flashing_format?
+     # sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
+   end
 
   # DELETE /resource/sign_out
   # def destroy
